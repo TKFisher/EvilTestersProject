af207da24bf02b0ed54c95e2290268a96b45c723
TKFisher <tracy.k.fisher@gmail.com>
2017-04-17 14:02:33 -0700

    Add browser test.

--- src/test/java/com/javafortesters/htmlunitdriver/startWebDriver.java
+++ src/test/java/com/javafortesters/htmlunitdriver/startWebDriver.java
@@ -3,17 +3,37 @@ package com.javafortesters.htmlunitdriver;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.assertTrue;
 
 
 public class startWebDriver {
 
-    
+
     @Test
     public void headlessDriver (){
 
          WebDriver driver = new HtmlUnitDriver();
          driver.navigate().to("http://www.compendiumdev.co.uk/selenium/");
+
+         assertTrue("The title starts as expected.", driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void browserDriverTest(){
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium/");
+
+        assertTrue("This title contains expected text.", driver.getTitle().contains("Automated Web Testing with Java"));
+
+        driver.close();
+        driver.quit();
 
     }
 
